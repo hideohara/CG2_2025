8655eb7a2512faac133e032117dce64b84abb7cc
ohara <ohara@g.neec.ac.jp>
2025-04-27 20:46:25 +0900

    01_03 DirectX12のオブジェクトを開放しよう

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 
+#include <dxgidebug.h>
+#pragma comment(lib, "dxguid.lib")
+
 
 void Log(const std::string& message) {
     OutputDebugStringA(message.c_str());
@@ -368,6 +371,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     Log(std::format("enemyHp:{}, texturePath:{}\n", 2, "aaaa"));
 
     Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+
+
+    CloseHandle(fenceEvent);
+    fence->Release();
+    rtvDescriptorHeap->Release();
+    swapChainResources[0]->Release();
+    swapChainResources[1]->Release();
+    swapChain->Release();
+    commandList->Release();
+    commandAllocator->Release();
+    commandQueue->Release();
+    device->Release();
+    useAdapter->Release();
+    dxgiFactory->Release();
+#ifdef _DEBUG
+    debugController->Release();
+#endif
+    CloseWindow(hwnd);
+
+    // リソースリークチェック
+    IDXGIDebug1* debug;
+    if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+        debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+        debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+        debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+        debug->Release();
+    }
+
+
 
     return 0;
 }
