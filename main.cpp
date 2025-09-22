aa82b819085a041562535fe8d27e108e50ee49d0
ohara <ohara@g.neec.ac.jp>
2025-09-22 15:50:33 +0900

    01_00 たくさんの板ポリを出そう_開始

--- main.cpp
+++ main.cpp
@@ -1138,8 +1138,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     */
 
     // モデル読み込み
-    ModelData modelData = LoadObjFile("resources", "fence.obj");
-    // ModelData modelData = LoadObjFile("resources", "plane.obj");
+    //ModelData modelData = LoadObjFile("resources", "fence.obj");
+    ModelData modelData = LoadObjFile("resources", "plane.obj");
     // ModelData modelData = LoadObjFile("resources", "axis.obj");
     // 頂点リソースを作る
     ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(VertexData) * modelData.vertices.size());
