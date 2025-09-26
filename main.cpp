4cc9e59cc99805e212a453e01c2c21181bf26790
ohara <ohara@g.neec.ac.jp>
2025-09-26 10:02:22 +0900

    01_01 Particleに動きをつけよう_移動

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 
 #include "externals/DirectXTex/d3dx12.h"
 
+#include <random>
+
 struct Vector4 {
     float x;
     float y;
@@ -66,6 +68,11 @@ struct Transform {
 struct TransformationMatrix {
     Matrix4x4 WVP;
     Matrix4x4 World;
+};
+
+struct Particle {
+    Transform transform;
+    Vector3 velocity;
 };
 
 // ----------------------------------------
@@ -269,6 +276,30 @@ Matrix4x4 Inverse(const Matrix4x4& m) {
 
     return result;
 }
+
+
+// 代入演算子オーバーロード
+// Vector3の足算
+Vector3& operator+=(Vector3& lhv, const Vector3& rhv) {
+    lhv.x += rhv.x;
+    lhv.y += rhv.y;
+    lhv.z += rhv.z;
+    return lhv;
+}
+
+// Vector3の掛け算
+Vector3& operator*=(Vector3& v, float s) {
+    v.x *= s;
+    v.y *= s;
+    v.z *= s;
+    return v;
+}
+
+const Vector3 operator*(const Vector3& v, float s) {
+    Vector3 temp(v);
+    return temp *= s;
+}
+
 
 // -------------------------
 
@@ -674,6 +705,18 @@ D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(ID3D12DescriptorHeap* descrip
     D3D12_GPU_DESCRIPTOR_HANDLE handleGPU = descriptorHeap->GetGPUDescriptorHandleForHeapStart();
     handleGPU.ptr += (descriptorSize * index);
     return handleGPU;
+}
+
+// パーティクル生成
+Particle MakeNewParticle(std::mt19937& randomEngine)
+{
+    std::uniform_real_distribution<float> distribution(-1.0f, 1.0f);
+    Particle particle;
+    particle.transform.scale = { 1.0f, 1.0f, 1.0f };
+    particle.transform.rotate = { 0.0f, 0.0f, 0.0f };
+    particle.transform.translate = { distribution(randomEngine), distribution(randomEngine), distribution(randomEngine) };
+    particle.velocity = { distribution(randomEngine), distribution(randomEngine), distribution(randomEngine) };
+    return particle;
 }
 
 
@@ -1421,12 +1464,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     //device->CreateShaderResourceView(instancingResource.Get(), &instancingSrvDesc, instancingSrvHandleCPU);
     device->CreateShaderResourceView(instancingResource, &instancingSrvDesc, instancingSrvHandleCPU);
 
-    Transform transforms[kNumInstance];
+    // 乱数
+    std::random_device seedGenerator;
+    std::mt19937 randomEngine(seedGenerator());
+
+    //Transform transforms[kNumInstance];
+    Particle particles[kNumInstance];
+    //for (uint32_t index = 0; index < kNumInstance; ++index) {
+    //    particles[index].transform.scale = { 1.0f, 1.0f, 1.0f };
+    //    particles[index].transform.rotate = { 0.0f, 0.0f, 0.0f };
+    //    particles[index].transform.translate = { index * 0.1f, index * 0.1f, index * 0.1f };
+    //    // 速度を上向きに設定
+    //    //particles[index].velocity = { 0.0f, 1.0f, 0.0f };
+    //    std::uniform_real_distribution<float> distribution(-1.0f, 1.0f);
+    //    // 位置と速度を[-1,1]でランダムに初期化
+    //    particles[index].transform.translate = { distribution(randomEngine), distribution(randomEngine), distribution(randomEngine) };
+    //    particles[index].velocity = { distribution(randomEngine), distribution(randomEngine), distribution(randomEngine) };
+    //}
+
     for (uint32_t index = 0; index < kNumInstance; ++index) {
-        transforms[index].scale = { 1.0f, 1.0f, 1.0f };
-        transforms[index].rotate = { 0.0f, 0.0f, 0.0f };
-        transforms[index].translate = { index * 0.1f, index * 0.1f, index * 0.1f };
+        particles[index] = MakeNewParticle(randomEngine);
     }
+
+
+    // Δtを定義。とりあえず60fps固定してあるが、実時間を計測して可変fpsで動かせるようにしておくとなお良い
+    const float kDeltaTime = 1.0f / 60.0f;
 
     // -----------------------------------------------------
 
@@ -1474,10 +1536,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
             Matrix4x4 worldViewProjectionMatrixSprite = Multiply(worldMatrixSprite, Multiply(viewMatrixSprite, projectionMatrixSprite));
             *transformationMatrixDataSprite = worldViewProjectionMatrixSprite;
 
+            // インスタンシング
             Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
             for (uint32_t index = 0; index < kNumInstance; ++index) {
+                particles[index].transform.translate += particles[index].velocity * kDeltaTime;
+
                 Matrix4x4 worldMatrix =
-                    MakeAffineMatrix(transforms[index].scale, transforms[index].rotate, transforms[index].translate);
+                    MakeAffineMatrix(particles[index].transform.scale, particles[index].transform.rotate, particles[index].transform.translate);
                 Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, viewProjectionMatrix);
                 instancingData[index].WVP = worldViewProjectionMatrix;
                 instancingData[index].World = worldMatrix;
