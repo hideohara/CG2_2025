88c175e72dfbb24d9679255400fab69826cafd85
ohara <ohara@g.neec.ac.jp>
2025-10-27 10:48:00 +0900

    開始

--- main.cpp
+++ main.cpp
@@ -1647,8 +1647,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
             // 描画！（DrawCall/ドローコール）。3頂点で1つのインスタンス。インスタンスについては今後
             // commandList->DrawInstanced(6, 1, 0, 0);
-            commandList->DrawInstanced(UINT(modelData.vertices.size()), numInstance, 0, 0);
-
+            if (numInstance > 0)
+            {
+                commandList->DrawInstanced(UINT(modelData.vertices.size()), numInstance, 0, 0);
+            }
             // -----------------------
 
             //// Spriteの描画。変更が必要なものだけ変更する
