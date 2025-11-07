6d46fc26594687d95b94edcf39b5ba713221135d
ohara <ohara@g.neec.ac.jp>
2025-11-07 09:34:34 +0900

    02_01 Blinn-Phong Reflection Model

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -43,10 +43,14 @@ PixelShaderOutput main(VertexShaderOutput input)
         
         float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
         float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
-        float RdotE = dot(reflectLight, toEye);
-        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
+        //float RdotE = dot(reflectLight, toEye);
+        //float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
         //float specularPow = pow(saturate(RdotE), 70); // 反射強度
+        float32_t3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+        float NDotH = dot(normalize(input.normal), halfVector);
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess);
 
+        
         //float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
         // half lambert
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
