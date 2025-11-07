e4e0b76d5e2ba6f300a6ef914eebc00a6b9d6c96
ohara <ohara@g.neec.ac.jp>
2025-11-07 10:22:24 +0900

    モデル表示

--- main.cpp
+++ main.cpp
@@ -24,6 +24,9 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 #include "externals/DirectXTex/DirectXTex.h"
 #include <numbers>
 
+#include <fstream>
+#include <sstream>
+
 struct Vector4 {
     float x;
     float y;
@@ -85,6 +88,19 @@ struct DirectionalLight {
 // カメラ
 struct CameraForGPU {
     Vector3 worldPosition;
+};
+
+
+// MaterialData
+struct MaterialData
+{
+    std::string textureFilePath;
+};
+
+// ModelData構造体
+struct ModelData {
+    std::vector<VertexData> vertices;
+    MaterialData material;
 };
 
 // ----------------------------------------
@@ -614,6 +630,122 @@ D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(ID3D12DescriptorHeap* descrip
 
 
 
+// mtlファイルを読む関数
+MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
+{
+    // 1. 中で必要となる変数の宣言
+    MaterialData materialData; // 構築するMaterialData
+    std::string line; // ファイルから読んだ1行を格納するもの
+    // 2. ファイルを開く
+    std::ifstream file(directoryPath + "/" + filename); // ファイルを開く
+    assert(file.is_open());  // とりあえず開けなかったら止める
+
+    // 3. 実際にファイルを読み、MaterialDataを構築していく
+    while (std::getline(file, line)) {
+        std::string identifier;
+        std::istringstream s(line);
+        s >> identifier;
+
+        // identifierに応じた処理
+        if (identifier == "map_Kd") {
+            std::string textureFilename;
+            s >> textureFilename;
+            // 連結してファイルパスにする
+            materialData.textureFilePath = directoryPath + "/" + textureFilename;
+        }
+    }
+
+    // 4. MaterialData を返す
+    return materialData;
+}
+
+// Objファイルを読む関数
+ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename)
+{
+    // 1. 中で必要となる変数の宣言
+    ModelData modelData; // 構築するModelData
+    std::vector<Vector4> positions; // 位置
+    std::vector<Vector3> normals; // 法線
+    std::vector<Vector2> texcoords; // テクスチャ座標
+    std::string line; // ファイルから読んだ1行を格納するもの
+
+    // 2. ファイルを開く
+    std::ifstream file(directoryPath + "/" + filename); // ファイルを開く
+    assert(file.is_open());  // とりあえず開けなかったら止める
+
+
+    // 3. 実際にファイルを読み、ModelDataを構築していく
+    while (std::getline(file, line)) {
+        std::string identifier;
+        std::istringstream s(line);
+        s >> identifier;// 先頭の識別子を読む
+
+        // identifierに応じた処理
+        if (identifier == "v") {
+            Vector4 position;
+            s >> position.x >> position.y >> position.z;
+            position.w = 1.0f;
+            positions.push_back(position);
+        }
+        else if (identifier == "vt")
+        {
+            Vector2 texcoord;
+            s >> texcoord.x >> texcoord.y;
+            texcoords.push_back(texcoord);
+        }
+        else if (identifier == "vn")
+        {
+            Vector3 normal;
+            s >> normal.x >> normal.y >> normal.z;
+            normals.push_back(normal);
+        }
+        else if (identifier == "f") {
+            VertexData triangle[3];
+            // 面は三角形限定。その他は未対応
+            for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+                std::string vertexDefinition;
+                s >> vertexDefinition;
+                // 頂点の要素へのIndexは「位置/UV/法線」で格納されているので、分解してIndexを取得する
+                std::istringstream v(vertexDefinition);
+                uint32_t elementIndices[3];
+                for (int32_t element = 0; element < 3; ++element) {
+                    std::string index;
+                    std::getline(v, index, '/');// /区切りでインデックスを読んでいく
+                    elementIndices[element] = std::stoi(index);
+                }
+                // 要素へのIndexから、実際の要素の値を取得して、頂点を構築する
+                Vector4 position = positions[elementIndices[0] - 1];
+                Vector2 texcoord = texcoords[elementIndices[1] - 1];
+                Vector3 normal = normals[elementIndices[2] - 1];
+                position.x *= -1.0f;
+                texcoord.y = 1.0f - texcoord.y;
+                // VertexData vertex = { position, texcoord, normal };
+                //VertexData vertex = { position, texcoord };
+                //modelData.vertices.push_back(vertex);
+                triangle[faceVertex] = { position, texcoord, normal };
+                //triangle[faceVertex] = { position, texcoord };
+            }
+            modelData.vertices.push_back(triangle[2]);
+            modelData.vertices.push_back(triangle[1]);
+            modelData.vertices.push_back(triangle[0]);
+        }
+        else if (identifier == "mtllib") {
+            // materialTemplateLibraryファイルの名前を取得する
+            std::string materialFilename;
+            s >> materialFilename;
+            // 基本的にobjファイルと同一階層にmtlは存在させるので、ディレクトリ名とファイル名を渡す
+            modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
+        }
+    }
+
+    // 4. ModelDataを返す
+    return modelData;
+}
+
+
+
+// -----------------------------------------------------
+
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
     if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
@@ -1025,7 +1157,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState));
     assert(SUCCEEDED(hr));
 
+
+    // -----------------------------------------------------------
     // 球表示用
+    /*
     const uint32_t kSubdivision = 12;
     const uint32_t kNumSphereVertices = kSubdivision * kSubdivision * 6;
     float pi = std::numbers::pi_v<float>;
@@ -1046,26 +1181,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     VertexData* vertexData = nullptr;
     // 書き込むためのアドレスを取得
     vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-/*
-    // 左下
-    vertexData[0].position = { -0.5f, -0.5f, 0.0f, 1.0f };
-    vertexData[0].texcoord = { 0.0f, 1.0f };
-    // 上
-    vertexData[1].position = { 0.0f, 0.5f, 0.0f, 1.0f };
-    vertexData[1].texcoord = { 0.5f, 0.0f };
-    // 右下
-    vertexData[2].position = { 0.5f, -0.5f, 0.0f, 1.0f };
-    vertexData[2].texcoord = { 1.0f, 1.0f };
-    // 左下2
-    vertexData[3].position = { -0.5f, -0.5f, 0.5f, 1.0f };
-    vertexData[3].texcoord = { 0.0f, 1.0f };
-    // 上2
-    vertexData[4].position = { 0.0f, 0.0f, 0.0f, 1.0f };
-    vertexData[4].texcoord = { 0.5f, 0.0f };
-    // 右下2
-    vertexData[5].position = { 0.5f, -0.5f, -0.5f, 1.0f };
-    vertexData[5].texcoord = { 1.0f, 1.0f };
-*/
 
     // 経度分割1つ分の角度
     const float kLonEvery = pi * 2.0f / float(kSubdivision);
@@ -1123,8 +1238,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
         }
     }
+    */
+    // -------------------------------------------------------
+
+    // モデル読み込み
+    ModelData modelData = LoadObjFile("resources", "plane.obj");
+    //ModelData modelData = LoadObjFile("resources", "axis.obj");
+    // 頂点リソースを作る
+    ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(VertexData) * modelData.vertices.size());
+    // 頂点バッファビューを作成する
+    D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+    vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();// リソースの先頭のアドレスから使う
+    vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());// 使用するリソースのサイズは頂点のサイズ
+    vertexBufferView.StrideInBytes = sizeof(VertexData);// 1頂点あたりのサイズ
+
+    // 頂点リソースにデータを書き込む
+    VertexData* vertexData = nullptr;
+    vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData)); // 書き込むためのアドレスを取得
+    std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());// 頂点データをリソースにコピー
 
 
+    // --------------------------------------------------
 
 
 
@@ -1448,7 +1582,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
             // 描画！（DrawCall/ドローコール）。3頂点で1つのインスタンス。インスタンスについては今後
             //commandList->DrawInstanced(6, 1, 0, 0);
-            commandList->DrawInstanced(kNumSphereVertices, 1, 0, 0);
+            //commandList->DrawInstanced(kNumSphereVertices, 1, 0, 0);
+            commandList->DrawInstanced(UINT(modelData.vertices.size()), 1, 0, 0);
 
             // -----------------------
 
